4aa5f8115a86d5a14b1f5c7aaa731ff78e3542c4
mweiss17 <martin.weiss@mail.mcgill.ca>
2014-04-24 22:40:18 -0400

    Draft of a new view controller to handle upcoming events. It sends a request to the web server and (currently my Dev, so change that for production!) and gets and parses a JSON with event info.

---
+++ upcomingEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  upcomingEventsViewController.h
+//  SDS
+//
+//  Created by Martin Weiss 1 on 2014-04-23.
+//  Copyright (c) 2014 Silent Disco Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface upcomingEventsViewController : UITableViewController
+
+@end
---
+++ upcomingEventsViewController.m
@@ -0,0 +1,135 @@
+//
+//  upcomingEventsViewController.m
+//  SDS
+//
+//  Created by Martin Weiss 1 on 2014-04-23.
+//  Copyright (c) 2014 Silent Disco Squad. All rights reserved.
+//
+
+#import "upcomingEventsViewController.h"
+
+@interface upcomingEventsViewController ()
+
+@end
+
+@implementation upcomingEventsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	NSString *fullURL = @"http://54.186.228.67/appindex.html ";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	// Get the data
+	NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *delegateFreeSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: self delegateQueue: [NSOperationQueue mainQueue]];
+
+
+    NSURLSessionDataTask * dataTask = [delegateFreeSession dataTaskWithURL:url
+                                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+															if(error == nil)
+															 {
+																 NSString * text = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+																 NSLog(@"Data = %@",text);
+																 
+																 NSData *jsonObject = [NSJSONSerialization JSONObjectWithData:[text dataUsingEncoding:NSUTF8StringEncoding]
+																													   options:0 error:NULL];
+																 NSLog(@"jsonObject=%@", jsonObject);
+																 
+
+															 }
+															 
+														 }];
+		
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
